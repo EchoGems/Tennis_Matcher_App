9ca590841e9dede095e96f013f26bc43c17c0256
learn <learn@learn12-2.local>
2017-12-08 13:06:08 -0800

    functioning skills option

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   get 'publics/home'
   get 'publics/profile'
-
   get 'publics/about'
+
   resources :matches
   root 'publics#home'
   get '/matches' => 'matches#index'
   get '/map' => 'matches#map'
+  post 'update' => 'publics#update'
 end
