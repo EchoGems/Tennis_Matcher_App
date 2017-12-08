5da069b822c8a632430f10e87ecb98e3df7f3507
learn <learn@learn12-2.local>
2017-12-07 16:28:19 -0800

    added links to our navbar, added about page, work in process for carousel

--- app/javascript/pages/home.js
+++ app/javascript/pages/home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 // import Nav from 'pages/nav.js'
 import PropTypes from 'prop-types';
-
+import {Router, Route} from 'react-router';
 import {Jumbotron, Button, Carousel, Item, Caption} from 'react-bootstrap'
 
 
@@ -11,27 +11,27 @@ export default class Home extends Component {
         return (
             <div className={"admin"}>
               <Jumbotron>
-                <h1>You found the home page</h1>
-                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1>It starts with LOVE</h1>
+                <p>You can use this app to find a local tennis player in San Diego and set up a date, time, and place to play!</p>
                 <p><Button bsStyle="primary">Learn more</Button></p>
               </Jumbotron>
               <Carousel>
                 <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                  <img width={900} height={500} alt="900x500" src="/assets/Serena.jpg" />
                   <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                  <img width={400} height={400} alt="900x500" src="/assets/nadal.jpg" />
                   <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                  <img width={900} height={500} alt="900x500" src="/assets/maria.jpeg" />
                   <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
