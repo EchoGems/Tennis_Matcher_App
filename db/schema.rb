6b2234a392565082232a387964e8913254d5f651
learn <learn@learn01.local>
2017-12-07 16:47:02 -0800

    added paperclip and skill level to profile view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207194305) do
+ActiveRecord::Schema.define(version: 20171207220829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20171207194305) do
     t.string "first_name"
     t.string "last_name"
     t.string "skill_level"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
