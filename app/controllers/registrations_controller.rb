89178b6aca361fbeb9c23f100557a84dc653f459
learn <learn@learn12-2.local>
2017-12-11 12:04:03 -0800

    added routes after successful log in/sign up and link to matches from profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  
+
   private
 
   def sign_up_params
@@ -9,5 +9,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :image, :password, :password_confirmation, :current_password)
+  end
+
+  # def new
+  #   render 'publics/profile.html.erb'
+  # end
+  def after_sign_up_path_for(resource)
+    '/publics/profile'
   end
 end
