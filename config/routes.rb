02fae15f58425a16a862c6e347a910b010c7a1e9
learn <learn@learn12-2.local>
2017-12-11 16:02:42 -0800

    Updated user profile and default image

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   root 'publics#home'
   get '/matches' => 'matches#index'
   get '/map' => 'matches#map'
-  post 'update' => 'publics#update'
+  post '/update_skill' => 'publics#update_skill'
+  post '/update_image' => 'publics#update_image'
 end
