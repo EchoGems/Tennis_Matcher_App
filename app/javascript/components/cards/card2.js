66975286217141b92eb6fd2a274ee3a3e6c696bc
learn <learn@learn18.local>
2017-12-12 12:02:10 -0800

    fixed cards

--- app/javascript/components/cards/card2.js
+++ app/javascript/components/cards/card2.js
@@ -3,19 +3,25 @@ import {Nav, Navbar, NavItem, NavDropdown, Collapse, Brand, Toggle, Header, Menu
 
 export default class Card2 extends Component {
 
-    render() {
-        return (
-          <div id="card-2"className="card text-white bg-info mb-3">
-            <div>
-              <img className="card-img" src="https://images.unsplash.com/photo-1511968410596-41f6437a0bbe?auto=format&fit=crop&w=967&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" alt="Card image"/>
-              <div className="card-img-overlay">
-                <div className="card-body">
-                  <h4 className="card-title">Find a Place to Play!</h4>
-                  <p className="card-text">Locate tennis courts that are convenient for the both of you.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-      )
-      }
+  render() {
+    var divStyle = {
+      width: "40rem",
+      height: "25rem",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPositionY:"bottom",
+      backgroundPositionX: "center",
+      backgroundImage: 'url("https://images.unsplash.com/photo-1511968410596-41f6437a0bbe?auto=format&fit=crop&w=967&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D")'
     }
+      return (
+        <div style={divStyle} id="card-1"className="card text-white bg-info mb-3">
+          <div className="card-content">
+            <h4 className="card-title">Find a Court</h4>
+            <p className="card-text">Locate tennis courts that are convenient for both of you.</p>
+          </div>
+        </div>
+
+
+    )
+    }
+  }
