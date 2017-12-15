df91ee6e71256d6e1074b1a04534beacf1a47b38
learn <learn@learn18.local>
2017-12-15 14:13:17 -0800

    fixed the about page

--- app/javascript/components/cards/card3.js
+++ app/javascript/components/cards/card3.js
@@ -5,7 +5,7 @@ export default class Card3 extends Component {
 
     render() {
       var divStyle = {
-        backgroundImage:'url("https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?auto=format&fit=crop&w=1050&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D")'
+        backgroundImage:'url("assets/time.jpg")'
       }
         return (
           <div style={divStyle} id="card-1"className="card text-white bg-info mb-3">
